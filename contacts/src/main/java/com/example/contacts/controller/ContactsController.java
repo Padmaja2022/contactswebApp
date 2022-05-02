de2745d5715a79b703e15a8c11cf9b61bfdc8d04
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-05-02 13:11:08 -0400

    Updated Controller to RestController

--- contacts/src/main/java/com/example/contacts/controller/ContactsController.java
+++ contacts/src/main/java/com/example/contacts/controller/ContactsController.java
@@ -10,13 +10,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.contacts.model.Contact;
 import com.example.contacts.model.Organization;
 import com.example.contacts.model.Person;
 import com.example.contacts.service.ContactsService;
 
-@Controller
+@RestController
 @RequestMapping("/contacts")
 public class ContactsController {
 	
